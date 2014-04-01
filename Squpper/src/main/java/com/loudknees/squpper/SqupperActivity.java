76f209431f5335cfd60a3b793dc615a3f78f0fb6
David Lutkin <genauw@gmail.com>
2014-04-01 14:40:58 +0100

    Added some javadoc stuff and sub-total for two exercise types

--- Squpper/src/main/java/com/loudknees/squpper/SqupperActivity.java
+++ Squpper/src/main/java/com/loudknees/squpper/SqupperActivity.java
@@ -12,16 +12,18 @@ import java.util.Random;
 
 public class SqupperActivity extends Activity {
 
-    int counter = 0;
+    int counter;
     int pushupTotal;
     int squatTotal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_squpper);
 
-        setExercise();
+        counter = 0;    //maybe change it so that it doesn't always start with even = push-ups
         setReps();
+        setExercise();
     }
 
 
@@ -45,22 +47,35 @@ public class SqupperActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Creates a random number from 1 - 13 inclusive
+     * @return random number as type String
+     * Adds number to running total for each exercise
+     */
     public String getNumber(){
         String stringReturnNumber;
         Random rand = new Random();
         int randomIndex = (rand.nextInt(13)) + 1;
+
+        subTotal(randomIndex);
+
         stringReturnNumber = Integer.toString(randomIndex);
         return stringReturnNumber;
     }
 
+    /**
+     * calls getNumber to get random number
+     * displays random number in TextView "textViewReps"
+     */
     public void setReps(){
         TextView reps = (TextView) findViewById(R.id.textViewReps);
         reps.setText(getNumber());
     }
 
     /**
-     * Change the name of the exercise in an alternating fasion
-     * When the counter is even, the exercise name should be "push-up"
+     * Change the name of the exercise in an alternating fashion
+     * When counter is even, exercise name will be displayed in TextView as "push-up"
+     * When counter is odd, exercise name will be displayed in TextView as "Squat"
      */
     public void setExercise(){
         TextView exerciseNames = (TextView)findViewById(R.id.textViewExerciseName);
@@ -80,9 +95,16 @@ public class SqupperActivity extends Activity {
         }
     }
 
+    /**
+     * Method initiated by button press from Button "nextButton"
+     * calls two methods "setReps()" and "setExercise()"
+     * changes two TextViews each time
+     * @param view
+     */
     public void nextSet(View view){
         setReps();
         setExercise();
+        //maybe create limit on how high counter can go
     }
 
     public void finish(View view){
