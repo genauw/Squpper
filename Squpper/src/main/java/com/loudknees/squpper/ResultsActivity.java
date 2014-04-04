5bda6a3a7cdc63c27f77e29ba256149289a20493
David Lutkin <genauw@gmail.com>
2014-04-04 07:24:46 +0200

    Add intent to get subtotal info

--- Squpper/src/main/java/com/loudknees/squpper/ResultsActivity.java
+++ Squpper/src/main/java/com/loudknees/squpper/ResultsActivity.java
@@ -1,6 +1,7 @@
 package com.loudknees.squpper;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,8 @@ public class ResultsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        Intent intent = getIntent();
     }
 
 
